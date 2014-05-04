c4f072b8dc470cb5e0a3b7d17053ac4935669e75
Inigo Lopez de Heredia <inigo.lheredia@gmail.com>
2014-05-04 10:48:03 +0100

    Add Undirected Graph implementation
    * Define simple, undirected graph struct and functions
    * Add unit test for all Graph functions
    * TODO: Create a common interface between Graph and DiGraph

--- graph.go
+++ graph.go
@@ -5,6 +5,151 @@ import (
 	"fmt"
 )
 
+// Attr is a set of attributes associated to a node/edge
+// Keys are strings. Values can be anything.
+type Attr struct {
+	attr map[string]interface{}
+}
+
+// NewAttr creates an empty set of attributes
+func NewAttr() *Attr {
+	attr := Attr{make(map[string]interface{})}
+	return &attr
+}
+
+// Set associates a new key-value pair
+func (attr *Attr) Set(key string, value interface{}) {
+	attr.attr[key] = value
+}
+
+// Get returns the associated value to the given key, and
+// a bool set to true if the key was found, false otherwise
+func (attr *Attr) Get(key string) (interface{}, bool) {
+	v, ok := attr.attr[key]
+	return v, ok
+}
+
+// Graph represents an Undirected Graph
+// TODO: Create a common interface for Graph and DiGraph
+type Graph struct {
+	nodes map[uint64]*Attr            // Nodes present in the Graph, with their attributes
+	edges map[uint64]map[uint64]*Attr // Adjacency list of edges, with their attributes
+}
+
+// NewGraph creates an empty Graph
+func NewGraph() *Graph {
+	return &Graph{
+		nodes: make(map[uint64]*Attr),
+		edges: make(map[uint64]map[uint64]*Attr),
+	}
+}
+
+// AddNode adds the given node to the Graph. If the nodeId
+// already exists, it will override its attributes
+func (g *Graph) AddNode(nodeId uint64, attr *Attr) {
+	if attr == nil {
+		attr = NewAttr()
+	}
+
+	g.nodes[nodeId] = attr
+	g.edges[nodeId] = make(map[uint64]*Attr)
+}
+
+// DeleteNode removes a node entry from the Graph.
+// Any edge associated with it will be removed too.
+func (g *Graph) DeleteNode(nodeId uint64) {
+	// Remove incident edges
+	for k := range g.edges[nodeId] {
+		delete(g.edges[k], nodeId)
+	}
+
+	delete(g.edges, nodeId)
+	delete(g.nodes, nodeId)
+}
+
+// AddEdge adds an edge (with its attributes) between nodes u and v
+// If the nodes don't exist, they will be automatically created.
+// If an u-v edge already existed, its attributes will be overridden
+func (g *Graph) AddEdge(u, v uint64, attr *Attr) {
+	if attr == nil {
+		attr = NewAttr()
+	}
+
+	// Add nodes if necessary
+	if _, ok := g.nodes[u]; !ok {
+		g.AddNode(u, nil)
+	}
+	if _, ok := g.nodes[v]; !ok {
+		g.AddNode(v, nil)
+	}
+
+	g.edges[u][v] = attr
+	g.edges[v][u] = attr
+}
+
+// DeleteEdge removes the u-v edge, if exists.
+// If any of the nodes don't exist, nothing happens
+func (g *Graph) DeleteEdge(u, v uint64) {
+	if _, ok := g.Node(u); ok {
+		if _, ok := g.Node(v); ok {
+			delete(g.edges[u], v)
+			delete(g.edges[v], u)
+		}
+	}
+}
+
+// Nodes returns the list of node ids (unsorted)
+func (g *Graph) Nodes() []uint64 {
+	nodes := make([]uint64, len(g.nodes))
+	n := 0
+	for k := range g.nodes {
+		nodes[n] = k
+		n++
+	}
+	return nodes
+}
+
+// Node returns the attributes associated with a given node, and
+// a bool flag set to true if the node was found, false otherwise
+func (g *Graph) Node(nodeId uint64) (*Attr, bool) {
+	attr, ok := g.nodes[nodeId]
+	return attr, ok
+}
+
+// Neighbors returns the list of nodes containing edges between the
+// given node and them.
+// An extra bool flag determines whether the node was found.
+func (g *Graph) Neighbors(nodeId uint64) ([]uint64, bool) {
+	if edges, ok := g.edges[nodeId]; ok {
+		nodes := make([]uint64, len(edges))
+
+		n := 0
+		for k := range edges {
+			nodes[n] = k
+			n++
+		}
+		return nodes, true
+	}
+	return nil, false
+}
+
+// Edge return the attributes associated with the u-v edge.
+// An extra bool flag determines whether the edge was found.
+func (g *Graph) Edge(u, v uint64) (*Attr, bool) {
+	if _, ok := g.Node(u); ok {
+		if _, ok := g.Node(v); ok {
+			edges, ok := g.edges[u][v]
+			return edges, ok
+		}
+	}
+	return nil, false
+}
+
+/*********************/
+/****** DiGraph ******/
+/*********************/
+
+// DiGraph represents a Directed Graph
 type DiGraph struct {
 	// Each node has a slice of edges
 	nodes map[uint64][]Edge
--- graph_test.go
+++ graph_test.go
@@ -4,6 +4,97 @@ import (
 	"testing"
 )
 
+func EqualsIntSlice(x, y []uint64) bool {
+	if len(x) != len(y) {
+		return false
+	}
+
+	for i, v := range x {
+		if v != y[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func TestGraphAddNode(t *testing.T) {
+	g := NewGraph()
+
+	g.AddNode(1, nil)
+
+	attr := NewAttr()
+	attr.Set("x", 25)
+	g.AddNode(2, attr)
+
+	if len(g.Nodes()) != 2 {
+		t.Errorf("Expected size %d, got %d", 2, len(g.Nodes()))
+	}
+
+	attr, _ = g.Node(2)
+	x, ok := attr.Get("x")
+	if !ok || x.(int) != 25 {
+		t.Errorf("Expected value: 25. Got %v", x)
+	}
+
+	// Update node
+	attr = NewAttr()
+	attr.Set("name", "Dylan")
+	g.AddNode(1, attr)
+
+	attr, _ = g.Node(1)
+	name, ok := attr.Get("name")
+	if !ok || name.(string) != "Dylan" {
+		t.Errorf("Expected value: 'Dylan'. Got %v", name)
+	}
+}
+
+func TestGraphDeleteNode(t *testing.T) {
+	g := NewGraph()
+	g.AddNode(1, nil)
+	g.AddNode(2, nil)
+	g.AddEdge(2, 1, nil)
+
+	g.DeleteNode(1)
+	if len(g.Nodes()) != 1 {
+		t.Errorf("Node was not successfully deleted")
+	}
+}
+
+func TestGraphAddEdge(t *testing.T) {
+	g := NewGraph()
+	attr := NewAttr()
+	attr.Set("x", 5)
+	g.AddEdge(2, 1, attr)
+
+	nodes, ok := g.Neighbors(1)
+	if !ok || !EqualsIntSlice(nodes, []uint64{2}) {
+		t.Errorf("Edge was not successfully added")
+	}
+	nodes, ok = g.Neighbors(2)
+	if !ok || !EqualsIntSlice(nodes, []uint64{1}) {
+		t.Errorf("Edge was not successfully added")
+	}
+
+	attr, ok = g.Edge(1, 2)
+	if !ok {
+		t.Errorf("Edge was not successfully added")
+	}
+	x, ok := attr.Get("x")
+	if !ok || x.(int) != 5 {
+		t.Errorf("Expected value: 5. Got %v", x)
+	}
+}
+
+func TestGraphDeleteEdge(t *testing.T) {
+	g := NewGraph()
+	g.AddEdge(2, 1, NewAttr())
+	g.DeleteEdge(1, 2)
+
+	if _, ok := g.Edge(2, 1); ok {
+		t.Errorf("Edge was not successfully deleted")
+	}
+}
+
 func TestNodes(t *testing.T) {
 	g := NewDiGraph()
 	g.AddNode(1)
