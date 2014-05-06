d9de8697a3a82ec0237464effa9782da81477679
Inigo Lopez de Heredia <inigo.lheredia@gmail.com>
2014-05-06 18:27:54 +0100

    Merge Graph and DiGraph in the same struct
    * Use Directed attribute to determine the nature of the Graph
    * Update search algorithms to take Graphs
    * Add mandatory weight attribute to Edge

--- search_test.go
+++ search_test.go
@@ -21,7 +21,7 @@ func equalPath(s1, s2 []uint64) bool {
 
 // TestDijkstra tests the Dijkstra algorithm with the sample test
 func TestDijkstra(t *testing.T) {
-	g := sampleDiGraph()
+	g := sampleGraph()
 
 	expected := []uint64{1, 2, 5, 8}
 	path, err := Dijkstra(g, 1, 8)
@@ -40,7 +40,7 @@ func TestDijkstra(t *testing.T) {
 // TestAstar tests the A* algorithm with an heuristic that
 // assigns more priority to numbers closer to 9
 func TestAStar(t *testing.T) {
-	g := sampleDiGraph()
+	g := sampleGraph()
 
 	h := func(node, goal uint64) int {
 		return (int)(goal - node)
@@ -59,7 +59,7 @@ func TestAStar(t *testing.T) {
 
 // TestBreathFirstSearch tests BreathFirstSearch algorithm with the sample test
 func TestBreathFirstSearch(t *testing.T) {
-	g := sampleDiGraph()
+	g := sampleGraph()
 
 	path, err := BreathFirstSearch(g, 1, 9)
 	if err != nil {
@@ -74,7 +74,7 @@ func TestBreathFirstSearch(t *testing.T) {
 
 // TestDepthFirstSearch tests DepthFirstSearch algorithm with the sample test
 func TestDepthFirstSearch(t *testing.T) {
-	g := sampleDiGraph()
+	g := sampleGraph()
 
 	path, err := DepthFirstSearch(g, 1, 9)
 	if err != nil {
@@ -87,54 +87,54 @@ func TestDepthFirstSearch(t *testing.T) {
 	}
 }
 
-// sampleDiGraph creates a simple DiGraph for testing purposes
-func sampleDiGraph() *DiGraph {
+// sampleGraph creates a simple Graph for testing purposes
+func sampleGraph() *Graph {
 	//  1  2  4
 	//  3  5  7
 	//  6  8  9
-	g := NewDiGraph()
-	g.AddNode(1)
-	g.AddNode(2)
-	g.AddNode(3)
-	g.AddNode(4)
-	g.AddNode(5)
-	g.AddNode(6)
-	g.AddNode(7)
-	g.AddNode(8)
-	g.AddNode(9)
+	g := NewGraph(true) // Directed Graph
+	g.AddNode(1, nil)
+	g.AddNode(2, nil)
+	g.AddNode(3, nil)
+	g.AddNode(4, nil)
+	g.AddNode(5, nil)
+	g.AddNode(6, nil)
+	g.AddNode(7, nil)
+	g.AddNode(8, nil)
+	g.AddNode(9, nil)
 
-	g.AddEdge(1, Edge{2, 1})
-	g.AddEdge(1, Edge{3, 1})
+	g.AddEdge(1, 2, 1, nil)
+	g.AddEdge(1, 3, 1, nil)
 
-	g.AddEdge(2, Edge{1, 1})
-	g.AddEdge(2, Edge{4, 1})
-	g.AddEdge(2, Edge{5, 1})
+	g.AddEdge(2, 1, 1, nil)
+	g.AddEdge(2, 4, 1, nil)
+	g.AddEdge(2, 5, 1, nil)
 
-	g.AddEdge(3, Edge{1, 1})
-	g.AddEdge(3, Edge{5, 1})
-	g.AddEdge(3, Edge{6, 1})
+	g.AddEdge(3, 1, 1, nil)
+	g.AddEdge(3, 5, 1, nil)
+	g.AddEdge(3, 6, 1, nil)
 
-	g.AddEdge(4, Edge{2, 1})
-	g.AddEdge(4, Edge{7, 1})
+	g.AddEdge(4, 2, 1, nil)
+	g.AddEdge(4, 7, 1, nil)
 
-	g.AddEdge(5, Edge{2, 1})
-	g.AddEdge(5, Edge{3, 1})
-	g.AddEdge(5, Edge{7, 1})
-	g.AddEdge(5, Edge{8, 1})
+	g.AddEdge(5, 2, 1, nil)
+	g.AddEdge(5, 3, 1, nil)
+	g.AddEdge(5, 7, 1, nil)
+	g.AddEdge(5, 8, 1, nil)
 
-	g.AddEdge(6, Edge{3, 1})
-	g.AddEdge(6, Edge{8, 1})
+	g.AddEdge(6, 3, 1, nil)
+	g.AddEdge(6, 8, 1, nil)
 
-	g.AddEdge(7, Edge{4, 1})
-	g.AddEdge(7, Edge{5, 1})
-	g.AddEdge(7, Edge{9, 1})
+	g.AddEdge(7, 4, 1, nil)
+	g.AddEdge(7, 5, 1, nil)
+	g.AddEdge(7, 9, 1, nil)
 
-	g.AddEdge(8, Edge{5, 1})
-	g.AddEdge(8, Edge{6, 1})
-	g.AddEdge(8, Edge{9, 1})
+	g.AddEdge(8, 5, 1, nil)
+	g.AddEdge(8, 6, 1, nil)
+	g.AddEdge(8, 9, 1, nil)
 
-	g.AddEdge(9, Edge{7, 1})
-	g.AddEdge(9, Edge{8, 1})
+	g.AddEdge(9, 7, 1, nil)
+	g.AddEdge(9, 8, 1, nil)
 
 	return g
 }
